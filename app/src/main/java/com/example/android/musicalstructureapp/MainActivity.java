b5e50693163d87c83c33e1986a384459af90b9aa
Larisa <l90vegeta@gmail.com>
2017-04-27 17:36:39 +0200

    Initial commit

---
+++ app/src/main/java/com/example/android/musicalstructureapp/MainActivity.java
@@ -0,0 +1,87 @@
+/*
+ * Copyright (C) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example.android.musicalstructureapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+
+import com.example.android.musicalstructureapp.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Set the content of the activity to use the activity_main.xml layout file
+        setContentView(R.layout.activity_main);
+
+        // Find the View that shows the playlists category
+        TextView playlists = (TextView) findViewById(R.id.playlists);
+        // Set a click listener on that View
+        playlists.setOnClickListener(new View.OnClickListener() {
+            // The code in this method will be executed when the numbers View is clicked on.
+            @Override
+            public void onClick(View view) {
+                Intent playlistsIntent = new Intent(MainActivity.this, PlaylistsActivity.class);
+                startActivity(playlistsIntent);
+            }
+        });
+
+        // Find the View that shows the artists category
+        TextView artists = (TextView) findViewById(R.id.artists);
+        // Set a click listener on that View
+       artists.setOnClickListener(new View.OnClickListener() {
+            // The code in this method will be executed when the numbers View is clicked on.
+            @Override
+            public void onClick(View view) {
+                Intent artistsIntent = new Intent(MainActivity.this, ArtistsActivity.class);
+                startActivity(artistsIntent);
+            }
+        });
+
+        // Find the View that shows the albums category
+        TextView albums = (TextView) findViewById(R.id.albums);
+        // Set a click listener on that View
+        albums.setOnClickListener(new View.OnClickListener() {
+            // The code in this method will be executed when the numbers View is clicked on.
+            @Override
+            public void onClick(View view) {
+                Intent albumsIntent = new Intent(MainActivity.this, AlbumsActivity.class);
+                startActivity(albumsIntent);
+            }
+        });
+
+        // Find the View that shows the songs category
+        TextView songs = (TextView) findViewById(R.id.songs);
+        // Set a click listener on that View
+        songs.setOnClickListener(new View.OnClickListener() {
+            // The code in this method will be executed when the numbers View is clicked on.
+            @Override
+            public void onClick(View view) {
+                Intent songsIntent = new Intent(MainActivity.this, SongsActivity.class);
+                startActivity(songsIntent);
+            }
+        });
+
+    }
+
+
+
+}
